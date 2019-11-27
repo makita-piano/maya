800ea8c01af601004f1bf2fede50ce91ede03773
hexyzaiza@gmail.com <makita@dev-vuejs-test.c.ptna-20181002.internal>
2019-11-27 20:43:54 +0000

    [29]

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,19 @@
     </VApp>
   </div>
 </template>
+
+<script>
+export default {
+  mounted(){
+    const title = "official website"
+    const description = "説明文"
+    document.title = title
+    document.querySelector("meta[property='og:title']")
+    　.setAttribute('content', title)
+    document.querySelector("meta[name='description']")
+    　.setAttribute('content', description)
+    document.querySelector("meta[property='og:description']")
+    　.setAttribute('content', description)
+  }
+}
+</script>
