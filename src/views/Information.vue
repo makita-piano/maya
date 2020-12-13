2fce2501cdcedc0b599bdb1bc818b29420289d97
makki! <41937003+makita-piano@users.noreply.github.com>
2020-12-13 18:05:24 +0900

    Update Information.vue

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -48,6 +48,8 @@
       　.setAttribute('content', description)
       document.querySelector("meta[property='og:description']")
       　.setAttribute('content', description)
+      document.querySelector("meta[property='og:url']")
+      　.setAttribute('content', "https://kagamimaya.klavier.page/information")
     },
 
     created(){
