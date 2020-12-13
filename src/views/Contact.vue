a0c7095c4165cdc832dc8fe859158501ea56b6ac
makki! <41937003+makita-piano@users.noreply.github.com>
2020-12-13 18:03:55 +0900

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -30,6 +30,8 @@
       　.setAttribute('content', description)
       document.querySelector("meta[property='og:description']")
       　.setAttribute('content', description)
+      document.querySelector("meta[property='og:url']")
+      　.setAttribute('content', "https://kagamimaya.klavier.page")
     }
   }
 </script>
