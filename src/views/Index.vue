400b410dffb910062f8a46cd00bc4f9bd3b1409e
makki! <41937003+makita-piano@users.noreply.github.com>
2021-03-10 14:05:59 +0900

    Update Index.vue

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -62,6 +62,19 @@
                 </a>
               </div>
             </li>
+            <li class="col-6_sm-12">
+              <div class="hatena">
+                <a href="https://mayakagami.hatenablog.com/" target="_blank"  class="grid">
+                  <div class="col-1 social-icon">
+                    <img src="hatenablog-logo.svg">
+                  </div>
+                  <div class="col social-text">
+                    <div>HatenaBlog</div>
+                    <div class="social-name">id:mayakagami</div>
+                  </div>
+                </a>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
